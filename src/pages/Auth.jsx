62a98b50a79f528e4df1faa881d4fb484d416d6d
ernest-jere <ernestmmjere@outlook.com>
2026-07-26 15:44:57 +0200

    updated app

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Auth() {
+
+    return (
+        <div>
+            <h1>Auth Page</h1>
+        </div>
+    )
+}
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Checkout() {
+    
+    return (
+        <div>
+            <h1>Checkout Page</h1>
+        </div>
+    )
+}
