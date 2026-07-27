611948a555b91bf1a32c36d6e994ea54b08f0c12
ernest-jere <ernestmmjere@outlook.com>
2026-07-27 21:33:23 +0200

    updated home page

---
+++ src/Data/products.js
@@ -0,0 +1,56 @@
+
+
+const products = [
+    {
+        id: 1,
+        name: "Wireless Headphones",
+        price: "99.99",
+        image: "https://images.pexels.com/photos/20385204/pexels-photo-20385204.jpeg",
+        description: "Premium wireless headphones with noice cancellation",
+    },
+
+    {
+        id: 2,
+        name: "Smart Watch",
+        price: "399.99",
+        image: "https://images.pexels.com/photos/31406903/pexels-photo-31406903.jpeg",
+        description: "Feature rich smart watch with fitness trucking",
+    },
+
+    {
+        id: 3,
+        name: "Laptop",
+        price: "679.99",
+        image: "https://images.pexels.com/photos/5793947/pexels-photo-5793947.jpeg",
+        description: "With graphics (GPU that processes visual data, imagery, and games.",
+    },
+
+    {
+        id: 4,
+        name: "Smart Phone",
+        price: "479.99",
+        image: "https://images.pexels.com/photos/11216260/pexels-photo-11216260.jpeg",
+        description: "With graphics (GPU that processes visual data, imagery, and games.",
+    },
+
+    {
+        id: 5,
+        name: "Smart Glasses",
+        price: "409.99",
+        image: "https://images.pexels.com/photos/8386429/pexels-photo-8386429.jpeg",
+        description: "Features front-facing lenses to capture hands-free photos and first-person video",
+    },
+
+    {
+        id: 6,
+        name: "Neckless",
+        price: "179.99",
+        image: "https://images.pexels.com/photos/19869441/pexels-photo-19869441.jpeg",
+        description: "With a continuous, symmetric strand of closely set diamonds, running the full length of the band.",
+    }
+]
+
+
+export function getProducts() {
+    return products;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,33 @@
-
+import { Link } from "react-router-dom";
+import { getProducts } from "../Data/Products";
 
 
 export default function Home() {
+    const products = getProducts();
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="page">
+            <div className="home-hero">
+                <h1 className="home-title">Welcome To ShopHub</h1>
+                <p className="home-subtitle">Discover Amazing Products At Great Prices</p>
+            </div>
+            <div className="container">
+                <h2 className="page-title">Our Products</h2>
+                <div className="products-grid">
+                    {products.map((product) => (
+                        <div className="product-card" key={product.id}>
+                            <img src={product.image} className="product-card-image" alt={product.name} />
+                            <div className="product-card-content">
+                                <h3 className="product-card-name">{product.name}</h3>
+                                <p className="product-card-price">{product.price}</p>
+                                <div className="product-card-actions">
+                                    <Link className="btn btn-secondary">View Detaile</Link>
+                                    <button className="btn btn-primary">Add To Cart</button>
+                                </div>
+                            </div>
+                        </div>                  
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
