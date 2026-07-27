c3a431a9d68cbe7b3beabeee717b180776dcec97
ernest-jere <ernestmmjere@outlook.com>
2026-07-28 00:13:48 +0200

    modified:   src/Data/products.js

--- src/Data/products.js
+++ src/Data/products.js
@@ -4,7 +4,7 @@ const products = [
     {
         id: 1,
         name: "Wireless Headphones",
-        price: "99.99",
+        price: "R99.99",
         image: "https://images.pexels.com/photos/20385204/pexels-photo-20385204.jpeg",
         description: "Premium wireless headphones with noice cancellation",
     },
@@ -12,31 +12,31 @@ const products = [
     {
         id: 2,
         name: "Smart Watch",
-        price: "399.99",
+        price: "R399.99",
         image: "https://images.pexels.com/photos/31406903/pexels-photo-31406903.jpeg",
-        description: "Feature rich smart watch with fitness trucking",
+        description: "Houses optical heart rate monitors, blood oxygen (SpO₂) sensors, and electrical sensors for ECG tracking.Compact Logic Board: Contains ultra-lo",
     },
 
     {
         id: 3,
         name: "Laptop",
-        price: "679.99",
+        price: "R679.99",
         image: "https://images.pexels.com/photos/5793947/pexels-photo-5793947.jpeg",
-        description: "With graphics (GPU that processes visual data, imagery, and games.",
+        description: "Packs high-end dedicated graphics cards and advanced cooling systems for rendering intensive 3D games.",
     },
 
     {
         id: 4,
         name: "Smart Phone",
-        price: "479.99",
-        image: "https://images.pexels.com/photos/11216260/pexels-photo-11216260.jpeg",
-        description: "With graphics (GPU that processes visual data, imagery, and games.",
+        price: "R479.99",
+        image: "https://images.pexels.com/photos/6388/apple-iphone-smartphone-technology.jpg",
+        description: "Includes high-resolution front and rear lenses capable of digital photography and 4K video recording Camera System",
     },
 
     {
         id: 5,
         name: "Smart Glasses",
-        price: "409.99",
+        price: "R409.99",
         image: "https://images.pexels.com/photos/8386429/pexels-photo-8386429.jpeg",
         description: "Features front-facing lenses to capture hands-free photos and first-person video",
     },
@@ -44,8 +44,8 @@ const products = [
     {
         id: 6,
         name: "Neckless",
-        price: "179.99",
-        image: "https://images.pexels.com/photos/19869441/pexels-photo-19869441.jpeg",
+        price: "R179.99",
+        image: "https://images.pexels.com/photos/20141640/pexels-photo-20141640.jpeg",
         description: "With a continuous, symmetric strand of closely set diamonds, running the full length of the band.",
     }
 ]
