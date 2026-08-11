80b1d33bd28c9b1dcfa31ea4ebcaec5966c3529a
ernest-jere <ernestmmjere@outlook.com>
2026-08-11 23:41:51 +0200

    changed import from products to products.js in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { getProducts } from "../Data/Products";
+import { getProducts } from "../Data/Products.js";
 import ProductCard from "../components/ProductCard";
 
 
